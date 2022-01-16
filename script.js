c1127dde9c3e48c213f451beffec69e7e5013636
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-01-16 21:48:49 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,21 @@
+function nextPage() {
+    window.location.href = "album1.html"
+}
+
+function nextPage1() {
+    window.location.href = "album2.html"
+}
+
+function nextPage2() {
+    window.location.href = "album3.html"
+}
+function nextPage3() {
+    window.location.href = "album4.html"
+}
+function nextPage4() {
+    window.location.href = "album5.html"
+}
+
+function nextPage5() {
+    window.location.href = "album6.html"
+}
